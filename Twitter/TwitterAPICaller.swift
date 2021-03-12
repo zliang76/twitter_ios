ab543a5951805b90721376d4b717246fb7533c61
Zifeng Liang <zifeng.liang@vanderbilt.edu>
2021-03-12 17:59:47 -0600

    fixed retweet api url

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -96,7 +96,7 @@ class TwitterAPICaller: BDBOAuth1SessionManager {
     }
     
     func retweet(tweetId:Int, success: @escaping () -> (), failure: @escaping (Error) -> ()) {
-        let url = "https://api.twitter.com/1.1/retweet/\(tweetId).json"
+        let url = "https://api.twitter.com/1.1/statuses/retweet/\(tweetId).json"
         TwitterAPICaller.client?.post(url, parameters: ["id":tweetId], progress: nil,
             success: {(task: URLSessionDataTask, response: Any?) in
             success()
